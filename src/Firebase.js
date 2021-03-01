356f520fa6b66c9d3cc4b63d389a9bf15eed183b
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-01 11:08:52 +0300

    firebase js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,5 +1,16 @@
 import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBnPW3JPzivDBo2kUOruEdIWljfy_8HnS4",
+  authDomain: "e-clone-e6bab.firebaseapp.com",
+  projectId: "e-clone-e6bab",
+  storageBucket: "e-clone-e6bab.appspot.com",
+  messagingSenderId: "1021158008371",
+  appId: "1:1021158008371:web:0b2ed957e9973eea6a2546",
+  measurementId: "G-NSKV5D5346",
+};
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
