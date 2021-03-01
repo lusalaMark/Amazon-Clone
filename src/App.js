2307f5fb0fa7219e631234d218965e5a446047ba
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-01 10:36:08 +0300

    App js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,18 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
-import Header from "./Header";
-import { CheckCircleOutlineRounded } from "@material-ui/icons";
-import Checkout from "./Checkout";
-
 function App() {
   return (
-    //BEM
-    <Router>
-      <div className="app">
-        <Header />
-
+    <div className="app">
+      <Router>
         <Switch>
-          <Route path="/checkout">
-            <Checkout />
-          </Route>
-
-          <Route path="/login">
-            <Login />
-          </Route>
-
           <Route path="/">
             <Home />
           </Route>
         </Switch>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
-
-useEffect(() => {
-  // will only run once when the app component loads...
-
-  auth.onAuthStateChanged((authUser) => {
-    console.log("THE USER IS >>> ", authUser);
-
-    if (authUser) {
-      // the user just logged in / the user was logged in
-
-      dispatch({
-        type: "SET_USER",
-        user: authUser,
-      });
-    } else {
-      // the user is logged out
-      dispatch({
-        type: "SET_USER",
-        user: null,
-      });
-    }
-  });
-}, []);
-
 export default App;
