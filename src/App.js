8e8b470d5ad980ade96ca56bf6ee2ef241d3475a
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-01 11:15:56 +0300

    adding user effect

--- src/App.js
+++ src/App.js
@@ -19,4 +19,27 @@ function App() {
     </div>
   );
 }
+
+useEffect(() => {
+  // will only run once when the app component loads...
+
+  auth.onAuthStateChanged((authUser) => {
+    console.log("THE USER IS >>> ", authUser);
+
+    if (authUser) {
+      // the user just logged in / the user was logged in
+
+      dispatch({
+        type: "SET_USER",
+        user: authUser,
+      });
+    } else {
+      // the user is logged out
+      dispatch({
+        type: "SET_USER",
+        user: null,
+      });
+    }
+  });
+}, []);
 export default App;
