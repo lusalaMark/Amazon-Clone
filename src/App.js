c9cb697e0d7dccd11e5223d13233f5bf4f160c32
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-01 10:03:47 +0300

    Amazon Clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import Header from "./Header";
+import { CheckCircleOutlineRounded } from "@material-ui/icons";
+import Checkout from "./Checkout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //BEM
+    <Router>
+      <div className="app">
+        <Header />
+
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+useEffect(() => {
+  // will only run once when the app component loads...
+
+  auth.onAuthStateChanged((authUser) => {
+    console.log("THE USER IS >>> ", authUser);
+
+    if (authUser) {
+      // the user just logged in / the user was logged in
+
+      dispatch({
+        type: "SET_USER",
+        user: authUser,
+      });
+    } else {
+      // the user is logged out
+      dispatch({
+        type: "SET_USER",
+        user: null,
+      });
+    }
+  });
+}, []);
 
 export default App;
